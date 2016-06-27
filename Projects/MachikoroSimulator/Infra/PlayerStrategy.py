abc8911fa142ca5b68da6aeb47d7c5ad397ef88c
Daniel <djscheufler@gmail.com>
2016-06-26 20:18:50 -0500

    Filled out PlayerStrategy
    Added CardCost Dictionary
    Implemented basic PlayerStrategy - buy cheapest next desired card
    Note: This strategy is known to be non-ideal

--- Projects/MachikoroSimulator/Infra/PlayerStrategy.py
+++ Projects/MachikoroSimulator/Infra/PlayerStrategy.py
@@ -2,24 +2,80 @@
 Purpose: The PlayerStrategy represnets, the desired final state of Owned CardEnum.
 
 """
-from .CardEnum import CardEnum
+from .CardEnum import *
 
 class PlayerStrategy():
     def __init__(self,desiredEndState=None):
         self.EndState = self._defaultEndState()
+        self.Deck = None
 
         if(desiredEndState != None):
             for key in desiredEndState.keys():
                 self.EndState[key] = desiredEndState[key]
 
+    def InjectDeck(self,deckManager):
+        self.Deck = deckManager
+
     def _defaultEndState(self):
         return {CardEnum.WheatField:1,CardEnum.Bakery:1,CardEnum.TrainStation:1,CardEnum.ShoppingMall:1,CardEnum.AmusementPark:1,CardEnum.RadioTower:1}
 
+    def GetNextPurchase(self,curState):
+        #get diffState
+        diffState = self._createDiffState(curState)
+
+        available = False
+        while( not available):
+            #get Cheapest next desired Card
+            nextGet = self._findCheapestNextDesired(diffState)
+            diffState[nextGet] = None # removes next cheapest from possibles, prevents inf loop
+
+            available = self._QueryCardAvailabile(nextGet)
+
+        return nextGet
+
+    def _createDiffState(self,curState):
+        diffState = {}
+        for key in self.EndState.keys():
+            curCnt = 0
+            try:
+                curCnt = curState[key]
+            except KeyError:
+                pass
+
+            diff = self.EndState[key] - curCnt
+            if(diff > 0):
+                diffState[key] = CardCosts[key]
+
+        return diffState
+
+    def _findCheapestNextDesired(self,diffState):
+        # search for min within in Dict
+        # may optimize later
+
+        nextCard = CardEnum.Undefined
+        minCost = 1e6 # Arbitrarily Large Number
+
+        for key in diffState.keys():
+            if(diffState[key] < minCost):
+                nextCard = key
+                minCost = diffState[key]
+
+        return nextCard
+
+        #[OTMZ] - tagged for optimization
+
+    def _QueryCardAvailabile(self, nextGet):
+        if(self.Deck is not None):
+            available = self.Deck.CArdAvailable(nextGet)
+        else:
+            available = True
+
+        return available
 
 class PlayerStategyFactory():
     def __inint__(self):
         pass
-        
+
     def CheeseFactoryStrategy(self):
         endState = {CardEnum.Ranch:3,CardEnum.CheeseFactory:3}
         return PlayerStrategy(endState)
--- Projects/MachikoroSimulator/Tests/Infra/PlayerStrategyTest.py
+++ Projects/MachikoroSimulator/Tests/Infra/PlayerStrategyTest.py
@@ -1,8 +1,7 @@
 import unittest
 
-from Infra.CardEnum import CardEnum
+from Infra.CardEnum import *
 import Infra.PlayerStrategy as strat
-
 
 class PlayerStrategyTest(unittest.TestCase):
     def setUp(self):
@@ -27,6 +26,34 @@ class PlayerStrategyTest(unittest.TestCase):
         #Assert
         ActualEndState = {self.cards.WheatField:1,self.cards.Bakery:1,self.cards.Ranch:3,self.cards.CheeseFactory:3,self.cards.TrainStation:1,self.cards.ShoppingMall:1,self.cards.AmusementPark:1,self.cards.RadioTower:1}
         self.assertEqual(ActualEndState,pStrat.EndState)
+
+    def test_ConstructDiffState(self):
+        #Arrange
+        endstate = {self.cards.Ranch:1}
+        pStrat = strat.PlayerStrategy(endstate)
+        curState = {self.cards.WheatField:1,self.cards.Bakery:1}
+        expected = {
+            self.cards.Ranch:CardCosts[self.cards.Ranch],
+            self.cards.TrainStation:CardCosts[self.cards.TrainStation],
+            self.cards.ShoppingMall:CardCosts[self.cards.ShoppingMall],
+            self.cards.AmusementPark:CardCosts[self.cards.AmusementPark],
+            self.cards.RadioTower:CardCosts[self.cards.RadioTower]
+            }
+        #Act
+        result = pStrat._createDiffState(curState)
+        #Assert
+        self.assertEqual(expected, result)
+
+    def test_WhatToBuyNext(self):
+        #Arrange
+        endstate = {self.cards.Ranch:1}
+        pStrat = strat.PlayerStrategy(endstate)
+        curState = {self.cards.WheatField:1,self.cards.Bakery:1}
+        #Act
+        result = pStrat.GetNextPurchase(curState)
+        #Assert
+        self.assertEqual(self.cards.Ranch, result)
+
 
 
 if __name__ == '__main__':
