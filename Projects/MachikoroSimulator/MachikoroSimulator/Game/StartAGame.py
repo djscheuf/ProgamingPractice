17b852507df4f017e841dc1760767da5d9bc94de
Daniel Scheufler <djscheufler@gmail.com>
2017-01-09 09:57:47 -0600

    Removing the old work
    SHould clear space and remove possible version drift.

--- Projects/MachikoroSimulator/MachikoroSimulator/Game/StartAGame.py
+++
@@ -1,7 +0,0 @@
-from .PlayerContext import PlayerContext
-
-
-class StartAGame:
-    @staticmethod
-    def with_(first, second):
-        return PlayerContext(first, second)
--- Projects/MachikoroSimulator/setup.py
+++
@@ -1,3 +0,0 @@
-"""
-This project runs in python 3. Beyond that it should not have any other dependencies.
-"""
